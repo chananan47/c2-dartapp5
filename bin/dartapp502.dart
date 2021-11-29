8ece8769757d3a871e04b3d35039543fb378d09c
chananan47 <namayura47@gmail.com>
2021-11-29 10:51:18 +0700

    initial commit

---
+++ bin/dartapp502.dart
@@ -0,0 +1,10 @@
+void main() {
+  final String name; //Statically type
+  const age = 36; //Statically type
+  var id = 12345; //Dynamically type
+  name = 'Steav';
+
+  print(name);
+  print(age);
+  print(id);
+}
